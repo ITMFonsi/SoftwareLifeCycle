c126251b4255c6c50a4baa62e619faed8a53572f
tscholy <cts6638@students.fhv.at>
2018-12-09 12:05:47 +0100

    hello

--- QuadraticEquation/src/main.java
+++ QuadraticEquation/src/main.java
@@ -25,18 +25,24 @@ public class main {
             for(int i = 0; i < results.size(); i++) {
                 System.out.format("%.10f %n", results.get(i));
             }
-
+           //Thread.sleep(1000);
         } catch (ImaginaryNumbersException e) {
             e.printStackTrace();
         } catch (AIsZeroException e) {
             e.printStackTrace();
         } catch (NotPlausibleException e) {
             e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
         } finally {
             long end_time = System.nanoTime();
             double difference = (end_time - start_time) / 1e6;
             System.out.println("Duration: (msec) ");
-            System.out.println(difference);
+            if(difference < 500) {
+                System.out.println(difference);
+            } else {
+                System.err.println(difference);
+            }
         }
 
     }
